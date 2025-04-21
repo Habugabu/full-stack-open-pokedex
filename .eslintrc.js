1b8bd0c8ec94a308692296cb80a442211adff5d8
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-04-21 17:22:22 +0900

    11.5-11.6 completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
     commonjs: true,
+    node: true,
     "jest/globals": true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
